1f976759c5915f2e0f37523eb3f0b04dce8bdbf5
Zuzu work <muratov01@mail.ru>
2023-04-20 23:26:50 +0600

    домашка 4

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+// let a=8
+// let b=6
+// if(a+b<15){
+//     console.log("Нет");
+// }
+
+// let a=8
+// let b=9
+// if(a+b>15){
+//     console.log("Да");
+// }
+
+
+
+
+
+
+
+
+console.log(17==='17');         // false
+let a = 7
+let b = 6
+console.log(a>=b);            // true
